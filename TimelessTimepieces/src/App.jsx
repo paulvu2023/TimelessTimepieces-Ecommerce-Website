cdc6025edef114ec1ee9891c1bc79cadc30672e3
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-12-02 14:04:07 -0800

    Add React Router

--- TimelessTimepieces/src/App.jsx
+++ TimelessTimepieces/src/App.jsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
+import Home from './ComponentsAndStyles/Home'
 
 function App() {
-
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
