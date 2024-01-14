89ea3a6ca04a499793ce19ee51d1ca0d49d1a8b0
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2024-01-13 19:54:11 -0800

    Add Patek Philippe home-card

--- TimelessTimepieces/src/components/Home.jsx
+++ TimelessTimepieces/src/components/Home.jsx
@@ -13,6 +13,16 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="home-card">
+        <div className="home-flex">
+          <div>
+            <h1>PATEK PHILIPPE</h1>
+            <h2>Founded in 1905 by watchmaker Hans Wilsdorf, Rolex crafts precision timepieces for dynamic lifestyles, serving athletes, explorers, and those in perpetual motion.</h2>
+            <Link className="home-link" to="">Find your Rolex</Link>
+          </div>
+          <img src="https://www.globalicejewelry.com/cdn/shop/collections/IMG_6070_1200x1200.jpg?v=1653999407" />
+        </div>
+      </div>
     </div>
   )
 }
