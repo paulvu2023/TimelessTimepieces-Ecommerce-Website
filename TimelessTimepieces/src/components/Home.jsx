712381671389c250d0609c6c9779977572eb2569
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2024-01-13 19:12:03 -0800

    Create Rolex home-card

--- TimelessTimepieces/src/components/Home.jsx
+++ TimelessTimepieces/src/components/Home.jsx
@@ -5,7 +5,12 @@ export default function Home() {
     <div className="home">
       <div className="home-card">
         <div className="home-flex">
-          
+          <img src="https://twaintime.com/cdn/shop/articles/Why_rolex_will_go_up_Blog.jpg?v=1668133304" />
+          <div>
+            <h1>ROLEX</h1>
+            <h2>Founded in 1905 by watchmaker Hans Wilsdorf, Rolex crafts precision timepieces for dynamic lifestyles, serving athletes, explorers, and those in perpetual motion.</h2>
+            <Link className="home-link" to="">Find your Rolex</Link>
+          </div>
         </div>
       </div>
     </div>
