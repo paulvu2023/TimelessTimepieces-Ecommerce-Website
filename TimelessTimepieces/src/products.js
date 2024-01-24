5da7eb7842bb0a59e11c3aaf631702f468c8b3e6
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2024-01-23 22:51:32 -0800

    Create mock database with arrays and dictionaries

---
+++ TimelessTimepieces/src/products.js
@@ -0,0 +1,171 @@
+export const PRODUCTS = [
+  // Each array is a list of watches from a particular watch brand
+  [
+    {
+      id: 1,
+      productName: "Yacht Master",
+      price: 4900,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 2,
+      productName: "Oyster Perpetual",
+      price: 7400,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 3,
+      productName: "Explorer",
+      price: 7800,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 4,
+      productName: "Submariner",
+      price: 9100,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 5,
+      productName: "Datejust",
+      price: 10500,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 6,
+      productName: "Submariner",
+      price: 11200,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 7,
+      productName: "Datejust",
+      price: 11500,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 8,
+      productName: "Datejust",
+      price: 12600,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 9,
+      productName: "Datejust",
+      price: 13800,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 10,
+      productName: "Datejust",
+      price: 13950,
+      productImage: "placeholder",
+    },
+  ],
+  // [
+  //   {
+  //     id: 11,
+  //     productName: "Oyster Perpetual",
+  //     price: 14200,
+  //     productImage: "placeholder",
+  //   },
+  // ],
+  [
+    {
+      id: 12,
+      productName: "Sea Dweller Deepsea",
+      price: 14500,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 13,
+      productName: "GMT-Master ii",
+      price: 19500,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 14,
+      productName: "Day Date",
+      price: 19500,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 15,
+      productName: "Daytona",
+      price: 20700,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 16,
+      productName: "Daytona",
+      price: 21400,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 17,
+      productName: "Daytona",
+      price: 21800,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 18,
+      productName: "Oyster Perpetual",
+      price: 25000,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 19,
+      productName: "Sea Dweller",
+      price: 29500,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 20,
+      productName: "Sky Dweller",
+      price: 35000,
+      productImage: "placeholder",
+    },
+  ],
+  [
+    {
+      id: 21,
+      productName: "Daytona",
+      price: 37000,
+      productImage: "placeholder",
+    },
+  ],
+];
