201560cfa5e1a9813e6ea7fa1b10e293fe9b80ef
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2024-01-28 17:01:36 -0800

    Add Rolex cards inside of the Rolex Page

--- TimelessTimepieces/src/components/RolexPage.jsx
+++ TimelessTimepieces/src/components/RolexPage.jsx
@@ -1,7 +1,12 @@
-export default function Rolex() {
+import { RolexData } from "../RolexData"
+import RolexCard from "./RolexCard"
+
+export default function RolexPage() {
   return (
     <>
-      <h1>testing</h1>
+      {RolexData.map((rolex) => (
+        <RolexCard key={rolex.id} id={rolex.id} productName={rolex.productName} price={rolex.price} productImage={rolex.productImage}/>
+      ))}
     </>
   )
 }
