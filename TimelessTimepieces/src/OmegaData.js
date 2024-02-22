5eca7bd04fdc65f40a5543127274a33332ece8ff
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2024-02-22 11:39:03 -0800

    Create OmegaData.js db

---
+++ TimelessTimepieces/src/OmegaData.js
@@ -0,0 +1,203 @@
+import image1 from "./assets/OmegaImages/1.png";
+import image2 from "./assets/OmegaImages/2.png";
+import image3 from "./assets/OmegaImages/3.png";
+import image4 from "./assets/OmegaImages/4.png";
+import image5 from "./assets/OmegaImages/5.png";
+import image6 from "./assets/OmegaImages/6.png";
+import image7 from "./assets/OmegaImages/7.png";
+import image8 from "./assets/OmegaImages/8.png";
+import image9 from "./assets/OmegaImages/9.png";
+import image10 from "./assets/OmegaImages/10.png";
+import image11 from "./assets/OmegaImages/11.png";
+import image12 from "./assets/OmegaImages/12.png";
+import image13 from "./assets/OmegaImages/13.png";
+import image14 from "./assets/OmegaImages/14.png";
+import image15 from "./assets/OmegaImages/15.png";
+import image16 from "./assets/OmegaImages/16.png";
+import image17 from "./assets/OmegaImages/17.png";
+import image18 from "./assets/OmegaImages/18.png";
+import image19 from "./assets/OmegaImages/19.png";
+import image20 from "./assets/OmegaImages/20.png";
+
+export const OmegaData = [
+  {
+    id: 1,
+    caseMaterial: "Stainless Steel",
+    caseSize: "38 MM",
+    dialColor: "Blue",
+    productName: "Speedmaster",
+    price: 5600,
+    productImage: image1,
+  },
+  {
+    id: 2,
+    caseMaterial: "Stainless Steel",
+    caseSize: "42 MM",
+    dialColor: "Blue",
+    productName: "Seamaster Diver 300",
+    price: 5900,
+    productImage: image2,
+  },
+  {
+    id: 3,
+    caseMaterial: "Stainless Steel",
+    caseSize: "38 MM",
+    dialColor: "Blue",
+    productName: "Seamaster Aqua Terra",
+    price: 6200,
+    productImage: image3,
+  },
+  {
+    id: 4,
+    caseMaterial: "Stainless Steel",
+    caseSize: "41 MM",
+    dialColor: "Blue",
+    productName: "De Ville Prestige",
+    price: 6300,
+    productImage: image4,
+  },
+  {
+    id: 5,
+    caseMaterial: "Stainless Steel",
+    caseSize: "42 MM",
+    dialColor: "Black",
+    productName: "Speedmaster Moonwatch",
+    price: 7600,
+    productImage: image5,
+  },
+  {
+    id: 6,
+    caseMaterial: "Stainless Steel",
+    caseSize: "43.5 MM",
+    dialColor: "White",
+    productName: "Seamaster Planet Ocean",
+    price: 7700,
+    productImage: image6,
+  },
+  {
+    id: 7,
+    caseMaterial: "Stainless Steel",
+    caseSize: "41 MM",
+    dialColor: "Blue",
+    productName: "Constellation",
+    price: 8000,
+    productImage: image7,
+  },
+  {
+    id: 8,
+    caseMaterial: "Stainless Steel",
+    caseSize: "43 MM",
+    dialColor: "Blue",
+    productName: "Speedmaster",
+    price: 9500,
+    productImage: image8,
+  },
+  {
+    id: 9,
+    caseMaterial: "Stainless Steel",
+    caseSize: "40.5 MM",
+    dialColor: "Black",
+    productName: "Speedmaster 57",
+    price: 10000,
+    productImage: image9,
+  },
+  {
+    id: 10,
+    caseMaterial: "Stainless Steel",
+    caseSize: "40.5 MM",
+    dialColor: "Blue",
+    productName: "Speedmaster 57",
+    price: 10000,
+    productImage: image10,
+  },
+  {
+    id: 11,
+    caseMaterial: "Stainless Steel",
+    caseSize: "45.5 MM",
+    dialColor: "Blue",
+    productName: "Seamaster Planet Ocean",
+    price: 14500,
+    productImage: image11,
+  },
+  {
+    id: 12,
+    caseMaterial: "Stainless Steel",
+    caseSize: "55 MM",
+    dialColor: "Blue",
+    productName: "Seamaster Ploprof",
+    price: 14600,
+    productImage: image12,
+  },
+  {
+    id: 13,
+    caseMaterial: "18K Rose Gold",
+    caseSize: "41 MM",
+    dialColor: "White",
+    productName: "Seamaster Aqua Terra",
+    price: 38750,
+    productImage: image13,
+  },
+  {
+    id: 14,
+    caseMaterial: "18K Rose Gold",
+    caseSize: "39 MM",
+    dialColor: "Blue",
+    productName: "Constellation",
+    price: 40100,
+    productImage: image14,
+  },
+  {
+    id: 15,
+    caseMaterial: "18K Rose Gold",
+    caseSize: "43 MM",
+    dialColor: "Brown",
+    productName: "Seamaster Aqua Terra",
+    price: 41800,
+    productImage: image15,
+  },
+  {
+    id: 16,
+    caseMaterial: "18K Rose Gold",
+    caseSize: "43 MM",
+    dialColor: "Silver",
+    productName: "Seamaster Aqua Terra",
+    price: 47000,
+    productImage: image16,
+  },
+  {
+    id: 17,
+    caseMaterial: "Gold",
+    caseSize: "42 MM",
+    dialColor: "Silver",
+    productName: "Speedmaster Moonwatch",
+    price: 57300,
+    productImage: image17,
+  },
+  {
+    id: 18,
+    caseMaterial: "18K White Gold",
+    caseSize: "42 MM",
+    dialColor: "Silver",
+    productName: "Speedmaster Moonwatch Professional",
+    price: 61000,
+    productImage: image18,
+  },
+  {
+    id: 19,
+    caseMaterial: "18K White Gold",
+    caseSize: "38.5 MM",
+    dialColor: "Black",
+    productName: "Speedmaster Calibre 321",
+    price: 88900,
+    productImage: image19,
+  },
+  {
+    id: 20,
+    caseMaterial: "18K White Gold",
+    caseSize: "42 MM",
+    dialColor: "Grey",
+    productName: "James Bond 007 60th Anniversary Seamaster Diver",
+    price: 164928,
+    productImage: image20,
+  },
+];
